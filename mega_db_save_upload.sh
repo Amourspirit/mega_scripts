8009791dfed2b581868a758610a7cd7c6463e285
Paul Moss <bigbytetech@gmail.com>
2018-06-10 00:15:54 -0400

    Update to mega_db_save_upload.sh 2018-06-10_12-15

--- mega_db_save_upload.sh
+++ mega_db_save_upload.sh
@@ -49,52 +49,76 @@
 #     /bin/bash /usr/local/bin/mega_db_save_upload.sh "user_fan" "user_fan_db" ~/.megarc_user_fan
 #
 # Exit Codes
-#     0 Normal Exit script found no issues
-#     20 No argument for user supplied for user
-#     21 No argument for user supplied for database
-#     30 File not found on mega.nz found
-#     41 If megamkdir had error creating directory on mega.nz
-#     42 If a file was found with the same name as part of the path on mega.nz
-#     50 ~/.my.cnf for mysqldump is not found or script does not have read premissions.
-#     51 ~/.megarc for mega.nz is not found or script does not have read premissions.
-#     52 The database passed in to the script does not exist
-#     53 The user passed in to the script does not exist
-#     100 There is another mega process running
-#     101 megarm not found. Megtools requires installing
-#     102 megals not found. Megtools requires installing
-#     103 megaput not found. Megtools requires installing
-#     104 no argument supplied for Mega Server Path
-#     110 no mega server path specified
-#     111 Optional argument two was passed in but the config can not be foud or we do not have read permissions
-#     112 The file to upload does not exist or can not gain read access.
-#     115 megamkdir not found. Megtools requires installing
+# Code    Definition
+#   0     Normal Exit script found no issues
+#  20     No argument for user supplied for user
+#  21     No argument for user supplied for database
+#  30     File not found on mega.nz
+#  40     If no argument is give for parameter one, the path to check.
+#  41     If megamkdir had error creating directory
+#  42     If a file was found with the same name as part of the path
+#  50     ~/.my.cnf for mysqldump is not found or script does not have read permissions.
+#  51     ~/.megarc for mega.nz is not found or script does not have read permissions.
+#  52     The database passed in to the script does not exist
+#  53     The user passed in to the script does not exist
+#  60     bzip2 not found
+# 100     There is another mega process running. Can not continue.
+# 101     megarm not found. Megtools requires installing
+# 102     megals not found. Megtools requires installing
+# 103     megaput not found. Megtools requires installing
+# 104     No argument supplied for Mega Server Path
+# 110     No mega server path specified
+# 111     Config can not be found or we do not have read permissions.
+# 112     The file to upload does not exist or can not gain read access.
+# 115     megamkdir not found. Megtools requires installing
 
-ENCRYPT_OUTPUT="yes"
-MEGA_ENABLED="yes"
-DELETE_BZ2_FILE="yes"
-MEGA_DELETE_OLD_BACKUPS="yes"
-MEGA_DELETE_LOCAL_FILE="yes"
+ENCRYPT_OUTPUT=true # true or false
+MEGA_ENABLED=true # true or false
+DELETE_BZ2_FILE=true # true or false
+MEGA_DELETE_OLD_BACKUPS=true # true or false
+DAYS_TO_KEEP_BACKUP=60 # number of days to keep backups for the current user on mega.nz
+DELETE_LOCAL_BACKUP=true # true or false
 # set SEND_EMAIL_ON_ERROR="yes" to send email on error. Also must have valid SEND_EMAIL_TO email address and SEND_EMAIL_FROM address
-SEND_EMAIL_ON_ERROR="no"
+SEND_EMAIL_ON_ERROR=false # true or false
 # 'SEND_EMAIL_TO' FOR MULTIPLE ADDRESS SEPERATE BY , SPACE
 # EXAMPLE: SEND_EMAIL_TO="myemail1@domain.com, myemail2@domain.com, myemail3@otherdomain.com"
-SEND_EMAIL_TO=""
+SEND_EMAIL_TO="" # must be a valid email address
 SEND_EMAIL_FROM="noreply@myserver.com"
-SEND_MAIL_CLIENT="/usr/sbin/sendmail"
 GPG_OWNER="bbserver"
 SERVER_NAME="bbserver"
 DB_USER="root"
 LOG_NAME="mega_db.log"
 LOG_ID="MEGA DATABASE: "
-DATELOG=`date +'%Y-%m-%d-%H-%M-%S'`
 LOG_SEP="=========================================${DATELOG}========================================="
+MEGA_DEL_OLD_NAME="mega_del_old.sh"
+MEGA_UPLOAD_FILE_NAME="mega_upload_file.sh"
+MEGA_EXIST_FILE_NAME="mega_dir_file_exist.sh"
+MEGA_MKDIR_FILE_NAME="mega_mkdir.sh"
+DATELOG=`date +'%Y-%m-%d-%H-%M-%S'`
 SYS_LOG_DIR="/var/log"
+SEND_MAIL_CLIENT="$(command -v sendmail)"
 SYS_LOG="$SYS_LOG_DIR/$LOG_NAME"
 THIS_SCRIPT=`basename "$0"`
+IS_SENDING_MAIL_ON_ERROR=false
 
+if [[ "$SEND_EMAIL_ON_ERROR" = true && -x "$(command -v sendmail)"  && -n "$SEND_EMAIL_TO" && -n "$SEND_EMAIL_FROM" ]]; then
+    IS_SENDING_MAIL_ON_ERROR=true
+fi
 # if log is not supplied then redirect to stdout
 if [[ -z $SYS_LOG ]]; then
   SYS_LOG=/dev/stdout
+fi
+
+if ! [ -x "$(command -v bzip2)" ]; then
+    echo "${DATELOG} ${LOG_ID}bzip2 not installed." >> ${LOG}
+    echo "${DATELOG} ${LOG_ID}You must install bzip2 to use '${THIS_SCRIPT}'. Exit Code: 60" >> ${LOG}
+    echo "${LOG_SEP}" >> ${SYS_LOG}
+    echo "" >> ${SYS_LOG}
+    if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
+        EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 5 $SYS_LOG_DIR/${LOG_NAME}) \n\n Log File: '${SYS_LOG}'")
+        ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
+    fi
+    exit 60
 fi
 
 if [ -z "$1" ]
@@ -103,7 +127,7 @@ if [ -z "$1" ]
     echo "${DATELOG} ${LOG_ID}No argument for user supplied for user! Exiting! Exit Code 20" >> ${SYS_LOG}
     echo "${LOG_SEP}" >> ${SYS_LOG}
     echo "" >> ${SYS_LOG}
-    if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+    if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
         EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 4 $SYS_LOG_DIR/${LOG_NAME}) \n\n Log File: '${SYS_LOG}'")
         ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
     fi
@@ -119,7 +143,7 @@ if [ $? -ne 0 ]; then
     echo "${LOG_SEP}" >> ${SYS_LOG}
     echo "" >> ${SYS_LOG}
 
-    if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+    if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
         EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 4 $SYS_LOG_DIR/${LOG_NAME}) \n\n Log File: '${SYS_LOG}'")
         ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
     fi
@@ -144,7 +168,7 @@ if [ -z "$2" ]
     echo "${DATELOG} ${LOG_ID}No argument for user supplied for database! Exit Code: 21" >> ${LOG}
     echo "${LOG_SEP}" >> ${LOG}
     echo "" >> ${LOG}
-    if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+    if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
         EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 5 ${LOG}) \n\n Log File: '${LOG}'")
         ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
     fi
@@ -156,28 +180,21 @@ DB_NAME="$2"
 DB_NAME_NEW=$DATELOG"_$DB_NAME"
 DB_FILE_SQL=$BAK_DIR/$DB_NAME_NEW".sql"
 DB_FILE=$DB_FILE_SQL".bz2"
-
-
-SYS_LOG="/var/log/mega_db.log"
-MSD="/usr/bin/mysqldump"
 LOCK_FILE="/tmp/mega_backup_db_lock"
-MEGA_DEL_OLD_NAME="mega_del_old.sh"
-MEGA_UPLOAD_FILE_NAME="mega_upload_file.sh"
-MEGA_EXIST_FILE_NAME="mega_dir_file_exist.sh"
-MEGA_MKDIR_FILE_NAME="mega_mkdir.sh"
 SCRIPT_DIR=$(dirname "$0")
 MEGA_BACKUP_DIR="/$SERVER_NAME/backups/${USER}/database"
 MEGA_DEL_OLD_SCRIPT=$SCRIPT_DIR"/"$MEGA_DEL_OLD_NAME
 MEGA_UPLOAD_FILE_SCRIPT=$SCRIPT_DIR"/"$MEGA_UPLOAD_FILE_NAME
 MEGA_EXIST_FILE_SCRIPT=$SCRIPT_DIR"/"$MEGA_EXIST_FILE_NAME
 MEGA_MKDIR_FILE_SCRIPT=$SCRIPT_DIR"/"$MEGA_MKDIR_FILE_NAME
-DAYS_TO_KEEP_BACKUP=60
-BASH="/bin/bash"
+MSD="$(command -v mysqldump)"
+BASH="$(command -v bash)"
 MYSQL_DIR="/var/lib/mysql"
 CURRENT_CONFIG=""
 HAS_CONFIG=0
 OUTPUT_FILE=$DB_FILE
-if [[ $ENCRYPT_OUTPUT = "yes" ]]; then
+
+if [[ "$ENCRYPT_OUTPUT" = true ]]; then
     OUTPUT_FILE=$OUTPUT_FILE".gpg"
 fi
 
@@ -192,7 +209,7 @@ if [[ -n "$3" ]]; then
         echo "${DATELOG} ${LOG_ID}Config file '${CURRENT_CONFIG}' does not exist or can not gain read access! Exit Code: 111" >> ${LOG}
         echo "${LOG_SEP}" >> ${LOG}
         echo "" >> ${LOG}
-        if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+        if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
             EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 4 ${LOG}) \n\n Log File: '${LOG}'")
             ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
         fi
@@ -210,7 +227,7 @@ if [ $? -ne 0 ]; then
     echo "${DATELOG} ${LOG_ID}'$DB_NAME' does Not Exit. Unable to continue: Exit Code: 52" >> ${LOG}
     echo "${LOG_SEP}" >> ${LOG}
     echo "" >> ${LOG}
-    if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+    if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
         EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 4 ${LOG}) \n\n Log File: '${LOG}'")
         ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
     fi
@@ -222,21 +239,21 @@ if [ $? -ne 0 ]; then
     echo "${DATELOG} ${LOG_ID}~/.my.cnf must be set up for MysqlDump to do its job. Unable to continue: Exit Code: 50" >> ${LOG}
     echo "${LOG_SEP}" >> ${LOG}
     echo "" >> ${LOG}
-    if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+    if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
         EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 4 ${LOG}) \n\n Log File: '${LOG}'")
         ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
     fi
     exit 50
 fi
 
-if [[ $MEGA_ENABLED = "yes" && $HAS_CONFIG -eq 0 ]]; then
+if [[ "$MEGA_ENABLED" = true && $HAS_CONFIG -eq 0 ]]; then
     # no config has been passed into script. check and see if the default exist
     test -r ~/.megarc
     if [ $? -ne 0 ];then
         echo "${DATELOG} ${LOG_ID}~/.megarc must be set up and readable by current to upload to mega when parameter 3 is omitted. Unable to continue: Exit Code: 51" >> ${LOG}
         echo "${LOG_SEP}" >> ${LOG}
         echo "" >> ${LOG}
-        if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+        if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
             EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 4 ${LOG}) \n\n Log File: '${LOG}'")
             ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
         fi
@@ -251,7 +268,7 @@ if [ $? -eq 0 ];then
     echo "${DATELOG} ${LOG_ID}There is another mega backup database process running! Exit Code: 100" >> ${LOG}
     echo "${LOG_SEP}" >> ${LOG}
     echo "" >> ${LOG}
-    if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+    if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
         EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 4 ${LOG}) \n\n Log File: '${LOG}'")
         ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
     fi
@@ -261,7 +278,7 @@ fi
 cd ${BAK_DIR}
 
 echo "${DATELOG} ${LOG_ID}Generating '${OUTPUT_FILE}' from database '${DB_NAME}'." >> ${LOG}
-if [[ $ENCRYPT_OUTPUT = "yes" ]]; then
+if [[ "$ENCRYPT_OUTPUT" = true ]]; then
     ${MSD} -u ${DB_USER} -h localhost -a ${DB_NAME} | bzip2  > ${DB_FILE} && gpg --encrypt --recipient "$GPG_OWNER" ${DB_FILE}
     echo "${DATELOG} ${LOG_ID}'${DB_FILE}' has been encrypted using gpg as'${OUTPUT_FILE}'." >> ${LOG}
 else
@@ -269,7 +286,7 @@ else
     echo "${DATELOG} ${LOG_ID}gpg encryption has been disabled in the script." >> ${LOG}
 fi
 echo "${DATELOG} ${LOG_ID}Generated '${OUTPUT_FILE}' from database '${DB_NAME}'." >> ${LOG}
-if [[ $DELETE_BZ2_FILE = "yes" ]]; then
+if [[ "$DELETE_BZ2_FILE" = true ]]; then
     rm -f ${DB_FILE}
     echo "${DATELOG} ${LOG_ID}Delete bz2 file is enabled and '${DB_FILE}' has been deleted from '${BAK_DIR}'." >> ${LOG}
 else
@@ -277,7 +294,7 @@ else
 fi
 
 # Create the path to upload to on mega if it does not exist
-if [[ $MEGA_ENABLED = "yes" ]]; then
+if [[ "$MEGA_ENABLED" = true ]]; then
     echo "${DATELOG} ${LOG_ID}Checking mega.nz path to see if it '$MEGA_BACKUP_DIR' directory exist. Will created if not." >> ${LOG}
     if [[ $HAS_CONFIG -eq 0 ]]; then
         # No argument is given for default configuration for that contains user account and password
@@ -292,13 +309,13 @@ if [[ $MEGA_ENABLED = "yes" ]]; then
         echo "${DATELOG} ${LOG_ID}There was an issue running script '$MEGA_MKDIR_FILE_NAME'! Exit Code: $EXIT_CODE" >> ${LOG}
         echo "${LOG_SEP}" >> ${LOG}
         rm -f "${LOCK_FILE}"
-        if [[ $SEND_EMAIL_ON_ERROR = "yes" && ! -z SEND_EMAIL_TO ]]; then
+        if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
             EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 4 ${LOG}) \n\n Log File: '${LOG}'")
             ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
         fi
         exit $EXIT_CODE
     fi
-    if [[ $MEGA_DELETE_OLD_BACKUPS = "yes" ]]; then
+    if [[ "$MEGA_DELETE_OLD_BACKUPS" = true ]]; then
          # Remove any expired database backup files
         if [[ $HAS_CONFIG -eq 0 ]]; then
             # No argument is given for default configuration for that contains user account and password
@@ -313,7 +330,7 @@ if [[ $MEGA_ENABLED = "yes" ]]; then
             echo "${DATELOG} ${LOG_ID}There was an issue running script '$MEGA_DEL_OLD_NAME'! Exit Code: $EXIT_CODE" >> ${LOG}
             echo "${LOG_SEP}" >> ${LOG}
             rm -f "${LOCK_FILE}"
-            if [[ $SEND_EMAIL_ON_ERROR = "yes" && ! -z SEND_EMAIL_TO ]]; then
+            if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
                 EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 3 ${LOG}) \n\n Log File: '${LOG}'")
                 ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
             fi
@@ -334,7 +351,7 @@ if [[ $MEGA_ENABLED = "yes" ]]; then
         echo "${DATELOG} ${LOG_ID}There was an issue running script '$MEGA_UPLOAD_FILE_NAME': Exit Code: $EXIT_CODE" >> ${LOG}
         echo "${LOG_SEP}" >> ${LOG}
         rm -f "${LOCK_FILE}"
-        if [[ $SEND_EMAIL_ON_ERROR = "yes" && ! -z SEND_EMAIL_TO ]]; then
+        if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
             EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 3 ${LOG}) \n\n Log File: '${LOG}'")
             ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
         fi
@@ -356,7 +373,7 @@ if [[ $MEGA_ENABLED = "yes" ]]; then
         echo "${DATELOG} ${LOG_ID}There was an issue running script '$MEGA_EXIST_FILE_NAME'. Script exited with code '$EXIT_CODE'! Exit Code: 30" >> ${LOG}
         echo "${LOG_SEP}" >> ${LOG}
         rm -f "${LOCK_FILE}"
-        if [[ $SEND_EMAIL_ON_ERROR = "yes" && -n SEND_EMAIL_TO ]]; then
+        if [[ "$IS_SENDING_MAIL_ON_ERROR" = true ]]; then
             EMAIL_MSG=$(echo -e "To: ${SEND_EMAIL_TO}\nFrom: ${SEND_EMAIL_FROM}\nSubject: ${SERVER_NAME} ${DATELOG} - ERROR RUNNING SCRIPT '${THIS_SCRIPT}' \n\n Log Tail:\n $(tail -n 3 ${LOG}) \n\n Log File: '${LOG}'")
             ${SEND_MAIL_CLIENT} -t <<< "$EMAIL_MSG"
         fi
@@ -364,7 +381,7 @@ if [[ $MEGA_ENABLED = "yes" ]]; then
     else
         echo "${DATELOG} ${LOG_ID}File ${OUTPUT_FILE_NAME} has made it onto mega.nz" >> ${LOG}
     fi
-    if [[ $MEGA_DELETE_LOCAL_FILE="yes" ]]; then
+    if [[ "$DELETE_LOCAL_BACKUP" = true ]]; then
         #delete local file
         rm -f "${OUTPUT_FILE}"
         echo "${DATELOG} ${LOG_ID}Local file ${OUTPUT_FILE_NAME} has been deleted." >> ${LOG}
